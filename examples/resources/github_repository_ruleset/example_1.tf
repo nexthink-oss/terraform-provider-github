40db9d9521a9def640a47a3566321001bb29317e
grahamhar <grahamhar@users.noreply.github.com>
2025-10-21 15:53:13 +0100

    feat: Add file path protection to rulesets (#2415)
    * feat: Add file path protection to rulesets
    
    * feat: Add file path protection to rulesets and tests
    
    * chore: Address PR comments
    
    * feat: Add repository max file size rule
    
    * feat: Add repository file extension restrictions
    
    * fix: correct resource type
    
    * docs: add new type to repository ruleset docs
    
    * Update github/resource_github_repository_ruleset_test.go
    
    Co-authored-by: Steve Hipwell <steve.hipwell@gmail.com>
    
    * Update website/docs/r/repository_ruleset.html.markdown
    
    Co-authored-by: Steve Hipwell <steve.hipwell@gmail.com>
    
    * Update website/docs/r/repository_ruleset.html.markdown
    
    * Apply suggestion from @nickfloyd
    
    ---------
    
    Co-authored-by: Nick Floyd <139819+nickfloyd@users.noreply.github.com>
    Co-authored-by: Steve Hipwell <steve.hipwell@gmail.com>

--- examples/resources/github_repository_ruleset/example_1.tf
+++ examples/resources/github_repository_ruleset/example_1.tf
@@ -33,6 +33,38 @@ resource "github_repository_ruleset" "example" {
       required_deployment_environments = ["test"]
     }
 
+    required_code_scanning {
+      required_code_scanning_tool {
+        alerts_threshold          = "errors"
+        security_alerts_threshold = "high_or_higher"
+        tool                      = "CodeQL"
+      }
+    }
+  }
+}
 
+# Example with push ruleset
+resource "github_repository_ruleset" "example_push" {
+  name        = "example_push"
+  repository  = github_repository.example.name
+  target      = "push"
+  enforcement = "active"
+
+  rules {
+    file_path_restriction {
+      restricted_file_paths = [".github/workflows/*", "*.env"]
+    }
+
+    max_file_size {
+      max_file_size = 100 # 100 MB
+    }
+
+    max_file_path_length {
+      max_file_path_length = 255
+    }
+
+    file_extension_restriction {
+      restricted_file_extensions = ["*.exe", "*.dll", "*.so"]
+    }
   }
 }
