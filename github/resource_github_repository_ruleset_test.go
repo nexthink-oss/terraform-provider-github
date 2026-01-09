0010c6abd65076b4f0776db4e5047c9d856ee377
Madhurjya Roy <madhurjya.roy@nexthink.com>
2026-01-09 12:44:46 +0530

    fix: remove paid plan organization test from ruleset tests

--- github/resource_github_repository_ruleset_test.go
+++ github/resource_github_repository_ruleset_test.go
@@ -456,9 +456,6 @@ func TestGithubRepositoryRulesets(t *testing.T) {
 		t.Run("with an individual account", func(t *testing.T) {
 			t.Skip("individual account not supported for this operation")
 		})
-		t.Run("with a paid plan in an organization", func(t *testing.T) {
-			testCase(t, organization)
-		})
 	})
 
 	t.Run("Creates repository ruleset with merge queue SQUASH method", func(t *testing.T) {
